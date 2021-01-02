722f4589358a021f1ce8cceae1f37acbc1dbcba1
Eatae <al-loco@mail.ru>
2021-01-02 18:40:00 +0300

    author CRUD

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,53 +9,111 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Author;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+
 class AuthorController extends AbstractController
 {
 
     /**
-     * @Route("/author", name="author")
+     * Show all (index)
+     *
+     * @Route("/author/show-all", name="author-show-all")
      */
-    public function actionIndex(): Response
+    public function actionShowAll(): Response
     {
-        return $this->render('author/index.html.twig', [
-            'controller_name' => 'AuthorController',
+        $repository = $this->getDoctrine()->getRepository(Author::class);
+
+        return $this->render('author/show-all.html.twig', [
+            'authors' => $repository->findAll(),
         ]);
     }
 
 
     /**
+     * Show one
+     *
+     * @Route("/author/show-one/{author}", name="author-show-one")
+     */
+    public function actionShowOne(Author $author): Response
+    {
+        return $this->render('author/show-one.html.twig', [
+            'author' => $author,
+        ]);
+    }
+
+
+    /**
+     * Create
+     *
      * @Route("/author/create", name="author-create")
      * Method ({"GET", "POST"})
      */
     public function actionCreate(Request $request, ValidatorInterface $validator): Response
     {
-
-        $repository = $this->getDoctrine()->getRepository(Author::class);
+        /* show form */
         if ($request->isMethod('GET')) {
             return $this->render('author/create.html.twig');
         }
+        $author = new Author();
+        $author->setName($request->request->get('name'));
+        $author->setEmail($request->request->get('email'));
+        $author->setGender($request->request->get('gender'));
 
-        /**** TEST ****/
-        //$em = $this->getDoctrine()->getManager();
-        dd($repository);
-        //dd($validator->validate($author));
-        return $this->render('author/create.html.twig');
+        /* validate, save */
+        if ( count($errors = $validator->validate($author)) > 0 ) {
+            return new Response((string)$errors);
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($author);
+        $em->flush();
+
+        return $this->redirectToRoute('author-show-all');
     }
 
 
     /**
-     * @Route("/author/update", name="author-update")
+     * Update
+     *
+     * @Route("/author/update/{author}", name="author-update")
+     * Method ({"GET", "POST"})
      */
-    public function actionUpdate(): Response
+    public function actionUpdate(Author $author, Request $request, ValidatorInterface $validator): Response
     {
+        /* show form */
+        if ($request->isMethod('GET')) {
+            return $this->render('author/update.html.twig', [
+                'author' => $author
+            ]);
+        }
+        $author->setName($request->request->get('name'));
+        $author->setEmail($request->request->get('email'));
+        $author->setGender($request->request->get('gender'));
 
+        /* validate */
+        if ( count($errors = $validator->validate($author)) > 0 ) {
+            return new Response((string)$errors);
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return $this->redirectToRoute('author-show-all');
     }
 
-    /**
-     * @Route("/author/delete", name="author-delete")
-     */
-    public function actionDelete(): Response
-    {
 
+    /**
+     * Delete
+     *
+     * @Route("/author/delete/{author}", name="author-delete")
+     * Method ({"POST"})
+     */
+    public function actionDelete(Author $author, Request $request): Response
+    {
+        /* show form */
+        if ($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($author);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('author-show-all');
     }
 }
